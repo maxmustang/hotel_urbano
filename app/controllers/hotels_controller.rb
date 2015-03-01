1754d0d1598837c06b769bc9a0624db131c390d6
Max Mustang <max.mustang@iclod.com>
2015-03-01 18:18:19 -0300

    Alteracao da action que vai renderizar o json de hoteis

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,5 +1,9 @@
 class HotelsController < ActionController::Base	
+	def find
+		@hotels = Hotel.find_on_period params[:search_term], Date.parse(params[:check_in]), Date.parse(params[:check_out])
+	end
+
 	def index
-		@hotels = Hotel.all
+		
 	end
 end
