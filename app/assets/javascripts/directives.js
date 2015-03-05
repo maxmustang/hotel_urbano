c59523227d07ae0d707bbe1dd56a46066ca5b0e3
Max Mustang <max.mustang@iclod.com>
2015-03-04 22:34:47 -0300

    Alteracoes de front

--- app/assets/javascripts/directives.js
+++ app/assets/javascripts/directives.js
@@ -1,6 +1,9 @@
 var hotelUrbano = angular.module('HotelUrbano', []);
 
 hotelUrbano.directive('datepicker', function() {
+  var today = new Date();
+  var minDate = today.getDay() + '/' + (today.getMonth() + 1)  +'/' + (today.getYear() + 1900)
+  console.log(minDate)
   return function(scope, element, attrs) {
     element.datepicker({
       dateFormat: 'dd/mm/yy',
@@ -8,9 +11,38 @@ hotelUrbano.directive('datepicker', function() {
       showAnim : 'clip',
       showSpeed : 'slow',
       showWeeks: true,
+      numberOfMonths:2,
+      minDate: minDate ,
       onSelect : function(dateText, elem){
         $(this).trigger('input');
       }
     });
    }
 });
+
+hotelUrbano.directive('autocomplete', function($http){
+    return function(scope, element, attrs){
+      element.autocomplete({
+        source: function(request, callback){
+          var url = 'http://localhost:3000/hotels/destination/' + scope.searchTerm
+          var list = []
+          $http.get(url).success(function(resp){
+            if(resp.not_found){
+              list.push(resp.not_found)
+            }else{
+              for (var i = 0; i < resp.hotels.length; i++) {
+                  list.push(resp.hotels[i].name);
+                };
+            }
+            callback(list)
+          })
+        },
+        select: function( event, ui ){
+          element.trigger('input');
+        },
+        change: function(event, ui){
+          element.trigger('input');
+        }
+      })
+    }
+})
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,15 +2,35 @@ angular.module('app', ['HotelUrbano']);
 
 function homeCtrl($scope, $http){
 	$scope.hotels = [];
-
+	$scope.checkIn = '09/03/2015';
+	$scope.checkOut = '16/03/2015';
+	$scope.searchTerm = 'cancun';
+		
 	$scope.findOnPeriod = function(){
-		$http.get('http://localhost:3000/hotels/in/cancun/from/14-03-2015/to/13-03-2015').success(function(resp){
+
+		var url = generetaUrl($scope.searchTerm, $scope.checkIn, $scope.checkOut);
+		$http.get(url).success(function(resp){
+			console.log(resp)
+			$scope.message = 'Buscando o melhor hotel pra vc :D'
 			if(resp.hotels.length > 0){
+				$scope.message = null
 				$scope.hotels = resp.hotels
 			}else{
-				$scope.oi = 'descupe, hotel ou cidade com nome Acapulco nao pode ser encontrado'
+				$scope.hotels = [];
+				$scope.message = 'descupe, hotel ou cidade com nome Acapulco nao pode ser encontrado'
 			}
 
 		});
 	}
 }
+
+function generetaUrl(searchTerm, checkIn, checkOut){
+	var parsedCheckIn = replaceAll(checkIn, '/', '-');
+	var parsedCheckOut = replaceAll(checkOut, '/', '-')
+	return 'http://localhost:3000/hotels/in/' + searchTerm + '/from/' + parsedCheckIn + '/to/'+ parsedCheckOut;
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
+
